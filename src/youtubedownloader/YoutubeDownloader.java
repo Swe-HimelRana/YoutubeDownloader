fa3c2f3062eaee1f6ce8880b8277898f2e49778f
Himel Rana <himel@localhost.localdomain>
2017-07-16 20:13:37 +0600

    uploading to github

---
+++ src/youtubedownloader/YoutubeDownloader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package youtubedownloader;
+
+/**
+ *
+ * @author himel
+ */
+public class YoutubeDownloader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gui g = new Gui();
+        g.setVisible(true);
+    }
+    
+}
---
+++ src/youtubedownloader/test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package youtubedownloader;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class test {
+    public static void main(String[] args) {
+        String UserName = System.getProperty("user.name");
+        System.out.println("/home/"+UserName+"/Videos/");
+    }
+}
